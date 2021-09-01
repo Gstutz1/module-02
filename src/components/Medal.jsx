b4f83d14a9c15276c3e4f4804871f8d693adc4a3
gstutz1 <gstutz1@my.wctc.edu>
2021-09-01 14:12:22 -0500

    added module-05

--- src/components/Medal.jsx
+++ src/components/Medal.jsx
@@ -1,13 +1,9 @@
-import React, {Component} from 'react';
 import {Typography} from '@material-ui/core'
 
-class Medal extends Component {
-    render() {
-        const {medalType, medalCount} = this.props;
+const Medal = ({medalType, medalCount}) => {
         return (
             <Typography variant='body1'>{medalType}s: {medalCount}</Typography>
         )
-    }
 }
 
 export default Medal;
